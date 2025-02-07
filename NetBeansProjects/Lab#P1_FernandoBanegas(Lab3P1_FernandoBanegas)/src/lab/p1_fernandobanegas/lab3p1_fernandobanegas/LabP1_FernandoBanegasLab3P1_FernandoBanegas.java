32daaa176e4daed95a59ed39584e0cd395e4f42b
Fernando <Fernando@localhost>
2025-02-07 15:38:13 -0600

    segundo commit

--- NetBeansProjects/Lab#P1_FernandoBanegas(Lab3P1_FernandoBanegas)/src/lab/p1_fernandobanegas/lab3p1_fernandobanegas/LabP1_FernandoBanegasLab3P1_FernandoBanegas.java
+++ NetBeansProjects/Lab#P1_FernandoBanegas(Lab3P1_FernandoBanegas)/src/lab/p1_fernandobanegas/lab3p1_fernandobanegas/LabP1_FernandoBanegasLab3P1_FernandoBanegas.java
@@ -26,33 +26,47 @@ public class LabP1_FernandoBanegasLab3P1_FernandoBanegas {
             int eje = entrada.nextInt();
             switch (eje){
                 case 1:
-                    System.out.println("Ingrese el tamano de el cuadrado");
-                    int tam = entrada.nextInt();
-                    if (tam>=5 && tam % 2 !=0){
-                        for (int fila = 1;fila<=tam;fila++){
-                            for (int col = 1;col<=tam;col++){
-                                for (int iz =1;iz<=tam;iz++){
-                                    for (int de=tam;tam>=1;de--){
-                                        for (int cont=1;cont<=tam;cont++){
-                                            int inicio=1;
-                                            
-                                        }
-                                    }
-                                }
-                                    
-                                    
-                                        
-                                        
-                                        
-                                    
-
-                                
-                            }
-                        }
+                    System.out.print("Ingrese un numero impar mayor a 5 : ");
+                    int num = entrada.nextInt();
+                    if (num>=5&&num%2!=0){
+                    for (int fila = 1; fila < num - 1; fila++){  
+                        for (int c = 0; c < num; c++) {
+                    if (c == 0 || fila == c || c == num - 1 || fila + c == num - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
                     }
+                }
+                System.out.println("");                    
+            }
+                        
+                    }
+                
                     break;
                 case 2:
-                    
+                    System.out.println("Permutaciones");
+                    System.out.print("Ingrese su valor n : ");
+                    int n = entrada.nextInt();
+                    System.out.print("Ingrese su valor r : ");
+                    int r = entrada.nextInt();
+                    if (n>=r){
+                       int numerador=1;
+                        for(int contador=1;contador<=n;contador++){
+                            numerador *= contador;
+                        }
+                        int resta = n-r;
+                        int denominador=1;
+                        for(int contador2=1;contador2<=resta;contador2++){
+                            denominador *= contador2;
+                        }
+                        
+                        int factorial = numerador/denominador;
+                        
+                        System.out.println("Permiutaciones posible con n= "+n+" y r= "+r+" es : "+factorial);
+                        
+                    }else{
+                        System.out.println("El valor r es mayor al valor n por lo tsnto no se puede realizar esta operacion");
+                    }
                     break;
                 case 3:
                     
